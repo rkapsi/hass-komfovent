e55b3fcd3816ecba0ae4b7485016e7d8b5adb807
Roger Kapsi <rkapsi@gmail.com>
2025-12-26 21:21:54 +0200

    Fix this

--- custom_components/komfovent/switch.py
+++ custom_components/komfovent/switch.py
@@ -24,7 +24,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
     return [
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_POWER,
+            register_id=registers.C6.REG_POWER.value,
             entity_description=SwitchEntityDescription(
                 key="power",
                 name="Power",
@@ -35,7 +35,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_ECO_MODE,
+            register_id=registers.C6.REG_ECO_MODE.value,
             entity_description=SwitchEntityDescription(
                 key="eco_mode",
                 name="ECO Mode",
@@ -46,7 +46,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_AUTO_MODE,
+            register_id=registers.C6.REG_AUTO_MODE.value,
             entity_description=SwitchEntityDescription(
                 key="auto_mode",
                 name="AUTO Mode",
@@ -57,7 +57,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_AQ_IMPURITY_CONTROL,
+            register_id=registers.C6.REG_AQ_IMPURITY_CONTROL.value,
             entity_description=SwitchEntityDescription(
                 key="aq_impurity_control",
                 name="AQ Impurity Control",
@@ -68,7 +68,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_AQ_HUMIDITY_CONTROL,
+            register_id=registers.C6.REG_AQ_HUMIDITY_CONTROL.value,
             entity_description=SwitchEntityDescription(
                 key="aq_humidity_control",
                 name="AQ Humidity Control",
@@ -79,7 +79,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_AQ_ELECTRIC_HEATER,
+            register_id=registers.C6.REG_AQ_ELECTRIC_HEATER.value,
             entity_description=SwitchEntityDescription(
                 key="aq_electric_heater",
                 name="AQ Electric Heater",
@@ -89,7 +89,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_ECO_FREE_HEAT_COOL,
+            register_id=registers.C6.REG_ECO_FREE_HEAT_COOL.value,
             entity_description=SwitchEntityDescription(
                 key="eco_free_heat_cool",
                 name="ECO Free Heating/Cooling",
@@ -100,7 +100,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_ECO_HEATER_BLOCKING,
+            register_id=registers.C6.REG_ECO_HEATER_BLOCKING.value,
             entity_description=SwitchEntityDescription(
                 key="eco_heater_blocking",
                 name="ECO Heater Blocking",
@@ -111,7 +111,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_ECO_COOLER_BLOCKING,
+            register_id=registers.C6.REG_ECO_COOLER_BLOCKING.value,
             entity_description=SwitchEntityDescription(
                 key="eco_cooler_blocking",
                 name="ECO Cooler Blocking",
@@ -122,7 +122,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_AWAY_HEATER,
+            register_id=registers.C6.REG_AWAY_HEATER.value,
             entity_description=SwitchEntityDescription(
                 key="away_electric_heater",
                 name="Away Electric Heater",
@@ -134,7 +134,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_NORMAL_HEATER,
+            register_id=registers.C6.REG_NORMAL_HEATER.value,
             entity_description=SwitchEntityDescription(
                 key="normal_electric_heater",
                 name="Normal Electric Heater",
@@ -146,7 +146,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_INTENSIVE_HEATER,
+            register_id=registers.C6.REG_INTENSIVE_HEATER.value,
             entity_description=SwitchEntityDescription(
                 key="intensive_electric_heater",
                 name="Intensive Electric Heater",
@@ -158,7 +158,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_BOOST_HEATER,
+            register_id=registers.C6.REG_BOOST_HEATER.value,
             entity_description=SwitchEntityDescription(
                 key="boost_electric_heater",
                 name="Boost Electric Heater",
@@ -170,7 +170,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_KITCHEN_HEATER,
+            register_id=registers.C6.REG_KITCHEN_HEATER.value,
             entity_description=SwitchEntityDescription(
                 key="kitchen_electric_heater",
                 name="Kitchen Electric Heater",
@@ -182,7 +182,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_FIREPLACE_HEATER,
+            register_id=registers.C6.REG_FIREPLACE_HEATER.value,
             entity_description=SwitchEntityDescription(
                 key="fireplace_electric_heater",
                 name="Fireplace Electric Heater",
@@ -194,7 +194,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_OVERRIDE_HEATER,
+            register_id=registers.C6.REG_OVERRIDE_HEATER.value,
             entity_description=SwitchEntityDescription(
                 key="override_electric_heater",
                 name="Override Electric Heater",
@@ -206,7 +206,7 @@ async def create_switches(coordinator: KomfoventCoordinator) -> list[KomfoventSw
         ),
         KomfoventSwitch(
             coordinator=coordinator,
-            register_id=registers.C6.REG_HOLIDAYS_HEATER,
+            register_id=registers.C6.REG_HOLIDAYS_HEATER.value,
             entity_description=SwitchEntityDescription(
                 key="holidays_electric_heater",
                 name="Holidays Electric Heater",
